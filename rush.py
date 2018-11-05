d73fefc53305ea9f2c343aac1de8134301ecdee8
Tobias <tobiasmaatita@outlook.com>
2018-11-05 16:46:09 +0100

    fried en jacob helpen ook

--- rush.py
+++ rush.py
@@ -20,14 +20,25 @@ class RushHour(object):
         return self.field
 
 
-    def load_vehicles(self, size, x, y, id, orientation):
+    def load_vehicles(self, car_size, x, y, id, orientation):
         vehicles = []
+
+        # weg als car.py wordt gebruikt
+        if y < 0:
+            return print("Error: y negative")
+        elif x < 0:
+            return print("Error: x negative")
 
         # horizontal
         if orientation.upper() == 'H':
 
             # if vehicle is of size 2, fill 2 spots
-            for i in range(size):
+            for i in range(car_size):
+
+                # vehicle cannot go off the board
+                # weg als car.py wordt gebruikt
+                if x + car_size - 1 > self.size:
+                    return print("Error too big")
 
                 # input y = 1: y-coordinate = 0 (left bottom)
                 y_car = self.size - y
@@ -40,7 +51,12 @@ class RushHour(object):
 
         # vertical
         if orientation.upper() == 'V':
-            for i in range(size):
+            for i in range(car_size):
+
+                # vehicle cannot go off the board
+                # weg als car.py wordt gebruikt
+                if y + car_size - 1 > self.size:
+                    return print("Error too big")
 
                 # input y = 1: y-coordinate = 0 (left bottom)
                 y_car = self.size - y - i
@@ -50,6 +66,7 @@ class RushHour(object):
 
                 # vehicle id on filled spot
                 self.field[y_car][x_car] = id
+                
 
         return self.field
 
@@ -63,7 +80,7 @@ class RushHour(object):
 if __name__ == "__main__":
     rush = RushHour(6)
     rush.load_field()
-    rush.load_vehicles(3, 1, 1, 1, 'v')
+    rush.load_vehicles(3, 1, 100, 1, 'v')
     plt.imshow(rush.field)
     plt.axis("off")
     plt.show()

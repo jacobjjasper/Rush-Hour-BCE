2fa864593454b4d0793c6458b0770eae3c3ceeec
Tobias <tobiasmaatita@outlook.com>
2018-12-07 16:29:43 +0100

    remove car truck

--- code/car.py
+++
@@ -1,9 +0,0 @@
-from vehicle import Vehicle
-
-class Car(Vehicle):
-    """
-    Car (child of Vehicle) class for Rush Hour game
-    """
-    def __init__(self, id, x, y, orientation):
-        Vehicle.__init__(self, id, x, y, orientation)
-        # self.size = 2
